1f48bed084738e46986c7b04f9f6694bf153b51d
Jesus Matute <mttrox@gmail.com>
2022-07-26 13:01:00 -0400

    fix

--- index.js
+++ index.js
@@ -2,7 +2,25 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("WORKING!!!");
+  res.send("Prueba tecnica realizada por Jesus Matute");
+});
+
+app.get("/iecho", (req, res) => {
+  try {
+    const { text } = req.query;
+    if (text) {
+      const reversedString = text.split("").reverse().join("");
+      if (text === reversedString) {
+        res.status(200).send({ text: reversedString, palindrome: true });
+      } else {
+        res.status(200).send({ text: reversedString });
+      }
+    } else {
+      res.status(400).send({ error: "no text" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(process.env.PORT || 5000);
